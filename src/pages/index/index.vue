59fcb021728ce48ba6ac5b51a0ef5d96dcfba230
hbrls <shuaizhexu@gmail.com>
2026-08-12 19:21:27 +0800

    feat: init app

---
+++ src/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="content">
+    <text class="title">Hello Preset Uniapp</text>
+  </view>
+</template>
+
+<style>
+.content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: calc(100vh - var(--window-top, 0px));
+}
+
+.title {
+  font-size: 64rpx;
+  font-weight: bold;
+}
+</style>
